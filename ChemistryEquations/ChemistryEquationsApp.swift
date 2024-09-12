c0a9390ae34cfdf0b87abf64c6e55be27b02b54f
Robby Sarvis <rjs.app.dev@proton.me>
2024-09-12 15:29:10 -0500

    Initial Commit

---
+++ ChemistryEquations/ChemistryEquationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemistryEquationsApp.swift
+//  ChemistryEquations
+//
+//  Created by Robert J. Sarvis Jr on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChemistryEquationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChemistryEquations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChemistryEquations
+//
+//  Created by Robert J. Sarvis Jr on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
