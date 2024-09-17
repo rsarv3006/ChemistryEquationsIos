3125c00622bd459e79dd70457d3fa3ef48f17ca6
Robby Sarvis <rjs.app.dev@proton.me>
2024-09-17 13:37:28 -0500

    initial commit

--- ChemistryEquations/ContentView.swift
+++ ChemistryEquations/ContentView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let thing = chemistry_equations_backbone.get_equations()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
         }
         .padding()
